82937ef657368e525872aed6f5109600e11f3c75
boutbitnz <boutbitnz2@gmail.com>
2018-01-28 22:07:49 -0600

    feature/ToDoApp: Added text-input component

--- ToDo/src/app/app.module.ts
+++ ToDo/src/app/app.module.ts
@@ -6,18 +6,24 @@ import { AppComponent } from './app.component';
 import { ToDoComponent } from './to-do/to-do.component';
 import { TodoService } from './to-do/to-do.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TextInputComponent } from './text-input/text-input.component';
+import { TextInputService } from './text-input/text-input.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToDoComponent
+    ToDoComponent,
+    TextInputComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [TodoService],
+  providers: [
+    TodoService,
+    TextInputService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
